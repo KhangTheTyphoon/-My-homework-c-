08acdeb6a9a36a791d3f4c0f717baefbedf3eea5
Lê Tiến Khang <khangthetyphoon@gmail.com>
2016-06-12 09:13:49 +0700

    Add files via upload

---
+++ stringde1.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include<string.h>
+using namespace std;
+
+struct thongtin
+{
+	char msts[30];
+	char hoten[30];
+	float diemthi;
+};
+//int main()
+//{
+//
+//	struct thongtin ts;
+//	strcpy_s(ts.msts, "STU09098");
+//	strcpy_s(ts.hoten, "Le Nguyen Trong Tuan Gia");
+//	ts.diemthi = 6.5;
+//	cout << "\nMa so cua thi sinh: " << ts.msts << endl;
+//	cout << "\nHo ten thi sinh: " << ts.hoten << endl;
+//	cout << "\nDiem thi :" << ts.diemthi << endl;
+//}
